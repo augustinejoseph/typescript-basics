ed824a98edea7977e5cc02aa62e6dbd365f80c87
augustinejoseph <augustine.augustine4@gmail.com>
2023-08-23 21:28:21 +0530

    first commit

---
+++ Basics/index.js
@@ -0,0 +1,14 @@
+var numbers = [1, 2, 3, 4, 5,];
+// numbers.push('lio')
+numbers.push(10);
+var characters = ['Mario', 'Luigi'];
+// characters.push(10)
+characters.push('Yasi');
+var mixedArray = ['Mario', 10];
+mixedArray.push(23);
+// mixedArray.push(true)
+// const emptyMixed : any[] = []
+var emptyMixed = [];
+emptyMixed.push('name');
+emptyMixed.push(39);
+emptyMixed.push(true);
---
+++ Basics/index.ts
@@ -0,0 +1,18 @@
+const numbers = [1,2,3,4,5,]
+// numbers.push('lio')
+numbers.push(10)
+
+const characters = ['Mario', 'Luigi']
+// characters.push(10)
+characters.push('Yasi')
+
+const mixedArray = ['Mario', 10]
+mixedArray.push(23)
+// mixedArray.push(true)
+
+
+// const emptyMixed : any[] = []
+const emptyMixed:any = []
+emptyMixed.push('name')
+emptyMixed.push(39)
+emptyMixed.push(true)
